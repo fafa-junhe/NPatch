0a7971773452bbcd7f49ad653b923e024e72a5b6
Berry Kovacek <842355358@qq.com>
2024-07-20 13:55:02 +0000

    test

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -49,6 +49,12 @@ android {
         jvmToolchain(17)
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+
     buildFeatures {
         compose = true
         buildConfig = true
